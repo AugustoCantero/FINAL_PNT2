fa26a19719d3a5662e67850ceca13cda72144ae9
cammidelgado <camiladelgado187@gmail.com>
2024-06-07 17:51:43 -0300

    Formulario para que el alumno pueda ver sus notas

--- src/componentes/Formulario/src/componentes/Formulario.js
+++ src/componentes/Formulario/src/componentes/Formulario.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 
 export default {
   name: 'src-componentes-formulario',
@@ -35,16 +36,37 @@ export default {
       !this.formData.email || 
       !this.formData.password
     },
-    enviar() {
+    async enviar() {
       const datos = {...this.formData}
       console.log(datos)
 
-      this.listarInscripto(datos)
-      this.formData = this.getInicialData()
-      this.formDirty = this.getInicialData()
+      const usuarioNotas = await this.getUsuarioNotas(datos.nombre)
+      if (usuarioNotas !== null) {
+        datos.notas = usuarioNotas || {notaMatematica: 'sin nota', notaLengua: 'sin nota', notaHistoria: 'sin nota'}
+        this.listarInscripto(datos)
+        this.formData = this.getInicialData()
+        this.formDirty = this.getInicialData()
+      } else {
+        alert('Usuario no encontrado')
+      }
+      
     },
     listarInscripto(datos){
       this.inscriptos.push(datos)
+    },
+    async getUsuarioNotas(nombre) {
+      try {
+        const response = await axios.get(`https://66562f2f9f970b3b36c490cd.mockapi.io/api/usuarios?nombre=${nombre}`)
+        const usuario = response.data[0]
+        return usuario ? {
+          notaMatematica: usuario.notaMatematica || 'sin nota',
+          notaLengua: usuario.notaLengua || 'sin nota',
+          notaHistoria: usuario.notaHistoria || 'sin nota'
+        } : null
+      } catch (error) {
+        console.log('Error al obtener las notas del usuario ', error)
+        return null
+      }
     }
   }
 }
