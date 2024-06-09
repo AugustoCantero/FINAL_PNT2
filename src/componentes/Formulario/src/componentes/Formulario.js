a122a8b5f95392c757fad8992af71fe3cabc6775
cammidelgado <camiladelgado187@gmail.com>
2024-06-09 19:05:58 -0300

    busca por email en vez de nombre

--- src/componentes/Formulario/src/componentes/Formulario.js
+++ src/componentes/Formulario/src/componentes/Formulario.js
@@ -40,23 +40,23 @@ export default {
       const datos = {...this.formData}
       console.log(datos)
 
-      const usuarioNotas = await this.getUsuarioNotas(datos.nombre)
+      const usuarioNotas = await this.getUsuarioNotas(datos.email)
       if (usuarioNotas !== null) {
         datos.notas = usuarioNotas || {notaMatematica: 'sin nota', notaLengua: 'sin nota', notaHistoria: 'sin nota'}
         this.listarInscripto(datos)
         this.formData = this.getInicialData()
         this.formDirty = this.getInicialData()
       } else {
-        alert('Usuario no encontrado')
+        alert('Usuario no encontrado, email incorrecto')
       }
       
     },
     listarInscripto(datos){
       this.inscriptos.push(datos)
     },
-    async getUsuarioNotas(nombre) {
+    async getUsuarioNotas(email) {
       try {
-        const response = await axios.get(`https://66562f2f9f970b3b36c490cd.mockapi.io/api/usuarios?nombre=${nombre}`)
+        const response = await axios.get(`https://66562f2f9f970b3b36c490cd.mockapi.io/api/usuarios?email=${email}`)
         const usuario = response.data[0]
         return usuario ? {
           notaMatematica: usuario.notaMatematica || 'sin nota',
